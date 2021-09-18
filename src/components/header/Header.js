6c8bdd4fb6fdebdb56d90eed523203f6ea9eae3e
Jmestrelozano <jmestrelozano@gmail.com>
2021-09-18 13:09:27 -0500

    agregando el build

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import  "../header/header.css"
-import banner from "../../bg.png"
 export const Header = ({children}) => {
     return (
         <div className="header">
             <div className="fondo">
-                <img className="fondo-img" src={banner} alt="fondo background"/>
+                <img className="fondo-img" src={process.env.PUBLIC_URL+'/assets/img/bg.png'} alt="fondo background"/>
             </div>
             
             <div className="logo">
